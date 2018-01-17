3fc32c333a5bcafb3be76755baa166e841da3a37
Azumi Callanan <azucm@hotmail.com>
2018-01-17 17:49:46 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  var 
+var i = 0;
 
+function init() {
+  var body = document.body.addEventListener('keydown', function(e){
+    var key = parselnt(e.detail || e.which);
+    if(key === code[i]){
+      index++;
+      if(i === code.length){
+        alert("Congrats!");
+        //reset the index counter
+        i = 0;
+      }
+    }else{
+      //if the key doesn't match current index, then reset it to zero
+      i = 0;
+    }
+  });
 }
